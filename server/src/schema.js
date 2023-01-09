b5e74fb202f9f2a1c109c87ef99c17f05f244a71
Machwusa <b.machwusa2gmail.com>
2023-01-09 09:14:04 +0200

    Part 4

--- server/src/schema.js
+++ server/src/schema.js
@@ -3,9 +3,24 @@ import { gql } from 'apollo-server';
 const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
-    tracksForHome: [Track!]!
+     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
-    track(id: ID!): Track!
+     track(id: ID!): Track!
+  }
+
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
